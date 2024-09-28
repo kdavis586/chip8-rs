ba1f98925e557def527f6ed8f1a973899a979c83
Kaelan Davis <kaelandavis01@gmail.com>
2024-09-27 17:46:19 -0700

    Start building cases for instructions

--- src/main.rs
+++ src/main.rs
@@ -8,30 +8,16 @@ struct Chip8 {
 
     keypad: u16,
 
-    stack: u16,
+    stack: [u16; 16],
     sp: u8, // stack pointer
 
     pc: u16, // program counter
 
     // registers
-    v0: u16,
-    v1: u16,
-    v2: u16,
-    v3: u16,
-    v4: u16,
-    v5: u16,
-    v6: u16,
-    v7: u16,
-    v8: u16,
-    v9: u16,
-    va: u16,
-    vb: u16,
-    vc: u16,
-    vd: u16,
-    ve: u16,
-    vf: u16,
+    v_reg: [u16; 16],
     i: u16,
 
+    // decrement @ 60hz
     dt: u8, // delay timer
     st: u8, // sound timer
 }
@@ -42,25 +28,10 @@ impl Default for Chip8 {
             ram: [0u8; 4096],
             disp_buffer: [0u8; 2048],
             keypad: 0,
-            stack: 0,
+            stack: [0u16; 16],
             sp: 0,
-            pc: 0x0200,
-            v0: 0,
-            v1: 0,
-            v2: 0,
-            v3: 0,
-            v4: 0,
-            v5: 0,
-            v6: 0,
-            v7: 0,
-            v8: 0,
-            v9: 0,
-            va: 0,
-            vb: 0,
-            vc: 0,
-            vd: 0,
-            ve: 0,
-            vf: 0,
+            pc: 0,
+            v_reg: [0u16; 16],
             i: 0,
             dt: 0,
             st: 0,
@@ -68,3 +39,59 @@ impl Default for Chip8 {
     }
 }
 
+impl Chip8 {
+    fn _cycle(&self) {
+        // fetch
+        let instruction: u16 = ((self.ram[self.pc as usize] as u16) << 4) | (self.ram[(self.pc + 1) as usize] as u16);
+
+        // decode 
+        match instruction & 0xF000 {
+            0x0000 => {
+                match instruction & 0x0F00 {
+                    0x0000 => {
+                        match instruction & 0x00FF {
+                            0x00E0 => {
+                                // TODO CLS
+                            },
+                            0x00EE => {
+                                // TODO RET
+                            },
+                            _ => {
+                                // TODO fail here
+                            }
+                        }
+                    },
+                    _ => {
+                        // TODO fail here
+                    }
+                }
+            },
+            0x1000 => {
+                // TODO JP 
+                let jp: u16 = instruction & 0x0FFF;
+            },
+            0x2000 => {
+                // TODO CALL
+            },
+            0x3000 => {
+                // TODO SE Vx
+            },
+            0x4000 => return,
+            0x5000 => return,
+            0x6000 => return,
+            0x7000 => return,
+            0x8000 => return,
+            0x9000 => return,
+            0xA000 => return,
+            0xB000 => return,
+            0xC000 => return,
+            0xD000 => return,
+            0xE000 => return,
+            0xF000 => return,
+            _ => {
+                // TODO fail here
+            }
+        }
+    }
+
+}
